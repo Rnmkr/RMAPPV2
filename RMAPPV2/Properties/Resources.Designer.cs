d5cb3060fb94c3e66bdee90c6fabab812aba41b0
Jose Pompilio <milveintiuno@gmail.com>
2021-01-18 10:02:56 -0300

    low volume clic added

--- RMAPPV2/Properties/Resources.Designer.cs
+++ RMAPPV2/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace RMAPPV2.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -106,6 +106,15 @@ namespace RMAPPV2.Properties {
             get {
                 object obj = ResourceManager.GetObject("deletetext", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream lv_click {
+            get {
+                return ResourceManager.GetStream("lv_click", resourceCulture);
             }
         }
         
